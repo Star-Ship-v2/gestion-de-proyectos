4e1d209764036557dbc8efea4d6868935fcb7266
KevinJaramilloGiraldo <kevin.jaramillo@correounivalle.edu.co>
2021-10-10 11:37:52 -0500

    login react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+  render (){
+    return (
+      <div className="App">
+     <div class="background"></div>
+     <div class="container">
+     <div class="login-container">
+       <div class="register">
+         <h1>Iniciar Sesión</h1>
+         <form action="">
+           <input type="text" placeholder="Nombre de usuario " class="nombre de usuario"/>
+           <input type="password" placeholder="Contraseña" class="pass"/>
+           <input type="submit" class="olvido" value="¿Has olvidado la contraseña?"/>
+           <input type="submit" class="submit" value="Iniciar"/>
+           <img src="img\chipBlue.PNG"/>
+          </form>
+       </div>
+       <div class="login">
+         <h2>Iniciar Sesión Con Redes Sociales</h2>
+         <div class="login-items">
+           <button class="fb"><i class="fab fa-facebook-f"></i> Acceder con Facebook</button>
+           <button class="tw"><i class="fab fa-twitter"></i> Acceder con Twitter</button>
+           <button class="correo"><i class="fas fa-envelope"></i> Acceder con Correo</button>
+         </div>
+       </div>
+     </div>
+   </div>
+      </div>
   );
+ }
 }
 
 export default App;
