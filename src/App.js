621a65213c400b77232c6aef85b8074f7b7ea800
KevinJaramilloGiraldo <kevin.jaramillo@correounivalle.edu.co>
2021-12-13 19:13:33 -0500

    Actualizacion

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import React from 'react';
 import Inicio from './pages/Inicio';
 import Credenciales from './pages/Credenciales';
 import Registro from './pages/Registro';
+import Actualizacion from './pages/Actualizacion';
 
 class App extends React.Component {
   render (){
     return (
-      <Registro/>
+      <Actualizacion/>
 
 
   );
